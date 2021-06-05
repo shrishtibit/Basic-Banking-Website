113fdfca26460b4004d5903fa515c6bfb38e8f9f
Shrishti Kumari <shristikumarigomia@gmail.com>
2021-06-05 16:33:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+		<title>HOME PAGE</title>
+		 <link rel="stylesheet" href="style.css">
+		 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+<body>
+	
+<div class="navbar">
+  <div class="logo">
+    <img src="logo.jpg" id="logoimg" height="60px" width="70px">
+  </div>
+<ul id="navlist">
+  <li class="navlist"><a class='active' href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+  <li class="navlist"><a href="customers.php"><i class='fa fa-users' aria-hidden='true'></i> View Customers</a></li>
+</ul>
+</div>
+  <div class="dark-overlay">
+  	<div>
+    <h1 class="title" style="color:white">Online Pay</h1>
+      <div class="subtitle" style="color:white">Easy Online Money Transfer<br></div>
+        <a href="customers.php" class="button1"><b>Transfer Money</b></a>
+  </div>
+ </div>
+<footer>
+  <p>Developed by Shrishti</p><br>
+  	<ul class="foot">
+  	Contact Me:
+   <li><a target="_blank" class="github" href="https://github.com/shrishtibit"><i class="fa fa-github fa-2x"></i></a></li>	
+   <li><a target="_blank" class="linkedin" href="https://www.linkedin.com/in/shrishti-kumari-51190216a/"><i class="fa fa-linkedin fa-2x"></i></a></li> 
+</ul>
+</footer>
+ </body>
+	 </html>
